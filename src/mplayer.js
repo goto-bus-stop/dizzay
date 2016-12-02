d671614625d6a7b4abacea67a10381cf88fbf174
René Kooi <renee@kooi.me>
2016-12-02 12:42:15 +0100

    Update plug-login usage

--- src/mplayer.js
+++ src/mplayer.js
@@ -12,7 +12,6 @@ const die = e => { throw e }
 // Plays YouTube and SoundCloud audio using mplayer.
 //
 export default function mplayer(plug, { mplayerArgs = [] }) {
-
   // horrible state
   let _instance
   const play = url => {
--- src/now-playing.js
+++ src/now-playing.js
@@ -3,12 +3,10 @@ import pluck from 'pluck'
 import compose from 'lodash.compose'
 
 export default function nowPlaying(plug) {
-
   const title = media => media && media.cid? `${media.author} - ${media.title}`
                        : /* otherwise */     `Nothing`
   const next = media => console.log(`[${time()}] Now Playing: ${title(media)}`)
 
   plug.on('advance',   compose(next, pluck('m')))
   plug.on('roomState', compose(next, pluck('playback.media')))
-
 }
