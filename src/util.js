ded689ca3a5609d305fcd3060197d0e276a4aca6
Renée Kooi <renee@kooi.me>
2017-05-04 00:15:49 +0200

    use ytdl, closes #3

--- src/util.js
+++ src/util.js
@@ -1,22 +1,45 @@
 const { exec } = require('child_process')
+const ytdl = require('ytdl-core')
 const debug = require('debug')('dizzay:util')
 
 // media source IDs
 const YOUTUBE    = 1
 const SOUNDCLOUD = 2
 
+function getYtUrl(url, { audioOnly, quality }, cb) {
+  ytdl.getInfo(url, (err, info) => {
+    if (err) return cb(err)
+    const fmt = info.formats.find((format) => {
+      if (audioOnly && format.audioBitrate && !format.bitrate) {
+        return true
+      } else if (!audioOnly && format.audioBitrate && format.bitrate) {
+        // TODO care about the video quality
+        return true
+      }
+      return false
+    })
+
+    if (fmt) cb(null, fmt.url)
+    else cb(new Error('No suitable format found'))
+  })
+}
+
 exports.getUrl = function getUrl(media, quality = 'best', cb) {
   const url = media.format === YOUTUBE
     ? `https://youtube.com/watch?v=${media.cid}`
     : `https://api.soundcloud.com/tracks/${media.cid}`
 
-  const params = (media.format === YOUTUBE
-    ? [ '-f', quality ]
-    : []).join(' ')
-
   debug('get-url', url)
 
-  exec(`youtube-dl --get-url ${params} ${url}`, (err, stdout, stderr) => {
+  if (media.format === YOUTUBE) {
+    return getYtUrl(url, {
+      audioOnly: quality.includes('audio'),
+      quality: quality
+    }, cb)
+  }
+
+  // TODO use soundcloud API for this one
+  exec(`youtube-dl --get-url ${url}`, (err, stdout, stderr) => {
     debug('get-url result', `${stdout}`)
     if (err) return cb(err)
     cb(null, `${stdout}`)
